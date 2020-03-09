3f474aec752a507c104f46159a1c5291fa8ecbec
AleksandrPuhov <aleksandr_puhov@list.ru>
2020-03-10 00:02:28 +0300

    finished first carousel

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,13 @@
 $(document).ready(function () {
 
-
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
+    $('.slider-carousel').owlCarousel({
         loop: true,
         margin: 10,
         responsiveClass: true,
         mouseDrag: false,
         items: 1,
-        nav: false,
-        dots: false
-    });
-
-    $('.btn-slider-left').click(function () {
-        owl.trigger('prev.owl.carousel');
-    });
-
-    $('.btn-slider-right').click(function () {
-        owl.trigger('next.owl.carousel');
+        dots: false,
+        nav:true
     });
 
 
